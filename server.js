14ba3b459cc25b12af7b464f24d32c55cec42976
Mariela Figueroa Salazar <mfigueroa@imsantiago.cl>
2026-02-20 16:23:55 -0300

    fix puerto render

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ app.get("/api/reportes", (req, res) => {
     res.json(reportes);
 });
 
-app.listen(3000, () => {
-    console.log("🚀 Sistema Operativo funcionando en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor funcionando en puerto " + PORT);
 });
