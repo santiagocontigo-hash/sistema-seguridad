60f3efff1014dbdadeae825b23564398d4e761af
Mariela Figueroa Salazar <mfigueroa@imsantiago.cl>
2026-02-23 17:16:33 -0300

    actualizacion formulario

---
+++ public/funcionarios.js
@@ -0,0 +1,26 @@
+const funcionarios = [
+
+/* =========================
+   MOVILES
+========================= */
+
+{ nombre: "JUAN MORENO ARIAS", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "CARLOS GONZALEZ PONTIGO", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "CARLOS JARVIS CRISTI", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "CLAUDIO MONTOYA VERA", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "MARIO TORO GONZALEZ", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "PAULINA ESPINOZA ZAPATA", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "CLAUDIO VILCHEZ BRAVO", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "DANIEL ARCE CONTRERAS", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "MARCOS MARABOLI PEREZ", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "IVANNA AYALA FALCON", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "MARCO SALINAS VELASQUEZ", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "RODRIGO VERGARA ROJAS", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "FERNANDO BRICEÑO YAÑEZ", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "ALEJANDRO MELITA CASTILLO", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+{ nombre: "HERNAN NOVOA PALMA", unidad: "MÓVILES", supervisor: "Darío Figueroa" },
+
+/* Agrega aquí el resto de MOVILES igual que arriba */
+/* (para no hacer este mensaje infinito, seguimos abajo) */
+
+];
